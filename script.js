3b622528516a4a0f54bfa691d340115b7b6830b4
ahnoukG <85653155+ahnoukG@users.noreply.github.com>
2024-11-05 17:30:22 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,69 @@
-script.js
+function show_Acaparadores() {
+            
+            window.scrollBy({
+                top: 300, 
+                behavior: 'smooth' 
+            });
+
+    var li_Acaparamiento = document.getElementById("li_Acaparamiento");
+	li_Acaparamiento.style.fontWeight = "normal";
+
+    var li_Acaparadores = document.getElementById("li_Acaparadores");
+	li_Acaparadores.style.fontWeight = "bold";
+
+	var content_Acaparadores = document.getElementById("content_Acaparadores");
+	content_Acaparadores.style.visibility = "visible";
+        }
+
+function show_Consecuencias() {
+            
+            window.scrollBy({
+                top: 300, 
+                behavior: 'smooth' 
+            });
+
+    var li_Acaparadores = document.getElementById("li_Acaparadores");
+	li_Acaparadores.style.fontWeight = "normal";
+
+    var li_Consecuencias = document.getElementById("li_Consecuencias");
+	li_Consecuencias.style.fontWeight = "bold";
+
+
+	var content_Consecuencias = document.getElementById("content_Consecuencias");
+	content_Consecuencias.style.visibility = "visible";
+        }
+
+function show_Acciones() {
+            
+            window.scrollBy({
+                top: 300, 
+                behavior: 'smooth' 
+            });
+
+    var li_Consecuencias = document.getElementById("li_Consecuencias");
+	li_Consecuencias.style.fontWeight = "normal";
+	
+    var li_Acciones = document.getElementById("li_Acciones");
+	li_Acciones.style.fontWeight = "bold";
+
+	
+	var content_Acciones = document.getElementById("content_Acciones");
+	content_Acciones.style.visibility = "visible";
+        }
+
+function show_Conclusion() {
+            
+            window.scrollBy({
+                top: 300, 
+                behavior: 'smooth' 
+            });
+
+    var li_Acciones = document.getElementById("li_Acciones");
+	li_Acciones.style.fontWeight = "normal";
+
+	var li_Conclusion = document.getElementById("li_Conclusion");
+	li_Conclusion.style.fontWeight = "bold";
+
+	var content_Conclusion = document.getElementById("content_Conclusion");
+	content_Conclusion.style.visibility = "visible";
+        }        
